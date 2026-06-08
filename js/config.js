c70ef082bf4953acdb50225d081af8e2c2de8773
javieroot <mac_javi@hotmail.com>
2026-06-08 17:26:48 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,17 @@
+
+window.PRONOSTIX_CONFIG = {
+  platformName: "Pronostix",
+  tournament: {
+    name: "Mundial FIFA 2026",
+    season: "2026",
+    icon: "🏆",
+    scoring: {
+      resultCorrect: 1,
+      exactScore: 1,
+      champion: 10,
+      runnerUp: 5,
+      thirdPlace: 3,
+      topScorer: 5
+    }
+  }
+};
