2172f18c8c0d7d0645e2bce7f788ea28cb8453a6
javieroot <mac_javi@hotmail.com>
2026-06-08 22:58:03 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
-function renderApp() {
+async function refreshCurrentUser() {
+  if (!currentUser) return;
+
+  const { data } = await client
+    .from("users")
+    .select("*")
+    .eq("id", currentUser.id)
+    .single();
+
+  if (data) {
+    setCurrentUser(data);
+  }
+}
+
+async function renderApp() {
   if (!currentUser) {
     renderAuth();
+    return;
+  }
+
+  await refreshCurrentUser();
+
+  if (currentUser.must_change_password) {
+    renderForcePasswordChange();
     return;
   }
 
